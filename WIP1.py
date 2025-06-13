f098d5b670d77b8ade039a385605072ea5c2e322
Jack Dolan <jacktdolan@gmail.com>
2025-06-13 12:39:54 -0600

    Created WIP1 file and pushed it to my repo.

---
+++ WIP1.py
@@ -0,0 +1,7 @@
+titles= ["Monty Python and the Holy Grail", "The Naked Gun", "Spaceballs"]
+for x in titles:
+    if x == "Monty Python and the Holy Grail":
+        print ("Movie title", x, "is correct")
+        break
+    else:
+        print ("Movie title", x, "is incorrect")
